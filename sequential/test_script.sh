eac83ac84ecd41670aac8bf918e3d9f2b710c9a1
KroupaS <kroupa400@centrum.cz>
2024-04-06 03:53:16 +0200

    typo

--- sequential/test_script.sh
+++ sequential/test_script.sh
@@ -7,7 +7,7 @@ success_all=true
 for file in $(ls -1 in_*.txt | sort); do
     # Run the program on each file
     # srun -p arm_long -c 48 ./vps.out "$file" -s
-    ./vps.out "$file$" -s
+    ./vps.out "$file" -s
     if [ $? -ne 0 ]; then
 	success_all=false
     fi
--- tasks_openmp/test_script.sh
+++ tasks_openmp/test_script.sh
@@ -6,7 +6,7 @@ success_all=true
 
 for file in $(ls -1 in_*.txt | sort); do
     # Run the program on each file
-    ./vps.out "$file$" -s
+    ./vps.out "$file" -s
     if [ $? -ne 0 ]; then
 	success_all=false
     fi
